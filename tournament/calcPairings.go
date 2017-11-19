fd4f4c9cd5f87e418c1cc1906c6286411e24f69a
autlunatic <autlunatic@gmail.com>
2017-11-19 22:10:31 +0100

    Added first tests with roundRobin

--- tournament/calcPairings.go
+++ tournament/calcPairings.go
@@ -1,24 +1,55 @@
 package tournament
 
 type pairing struct {
-	contributor1 *Contributor
-	contributor2 *Contributor
+	contributor1 Contributor
+	contributor2 Contributor
 }
 
 func calcPairingsGroup(contributors Contributors) []pairing {
 	var result []pairing
-	for _, c1 := range contributors.items {
-		for _, c2 := range contributors.items {
-			if c1 != c2 {
-				addpair(&result, &c1, &c2)
-			}
-		}
+
+	// make a copy of the contributors
+	tempContributors := make([]Contributor, len(contributors.items))
+	copy(tempContributors, contributors.items)
+
+	// if the count is odd add one dummy contributor for the roundRobin
+	if len(contributors.items)%2 > 0 {
+		tempContributors = append(tempContributors, Contributor{""})
+	}
+	// cut the first fixated contributor see roundrobin
+	if len(tempContributors) > 1 {
+		tempContributors = append(tempContributors[1:])
 	}
 
+	halfContributorCount := len(tempContributors) / 2
+	for i := 0; i < len(tempContributors); i++ {
+		c1 := contributors.items[0]
+
+		for j := 0; j < halfContributorCount+1; j++ {
+			if j == 0 {
+				addPair(&result, c1, tempContributors[halfContributorCount])
+			} else {
+				addPair(&result, tempContributors[j-1], tempContributors[j+halfContributorCount])
+			}
+		}
+		tempContributors = append(tempContributors[1:],tempContributors[0])
+	}
+
+	/*	for _, c1 := range contributors.items {
+			for _, c2 := range tempContributors {
+				if c1 != c2 {
+					addPair(&result, c1, c2)
+				}
+			}
+		}
+	*/
 	return result
 
 }
-func addpair(pairings *[]pairing, contributor *Contributor, contributor2 *Contributor) {
-	pair := pairing{contributor, contributor2}
+func addPair(pairings *[]pairing, contributor1 Contributor, contributor2 Contributor) {
+	if contributor1.name == "" || contributor2.name == "" {
+		return
+	}
+	pair := pairing{contributor1, contributor2}
 	*pairings = append(*pairings, pair)
 }
--- tournament/calcPairings_test.go
+++ tournament/calcPairings_test.go
@@ -12,8 +12,38 @@ func TestCalcPairings(t *testing.T) {
 	contributors.items = append(contributors.items, benni)
 	contributors.items = append(contributors.items, dani)
 
-	pairingCount := calcPairingsGroup(contributors)
+	pairings := calcPairingsGroup(contributors)
 
-	TestingUtils.CheckEquals(2, len(pairingCount), t)
+	TestingUtils.CheckEquals(1, len(pairings), t)
+	pair := pairings[0]
 
+	if (pair.contributor1.name != "Benni") || (pair.contributor2.name != "Dani") {
+		t.Errorf("first contriburor should be Benni (but was %s) and second should be Dani (but was %s)", pair.contributor1.name, pair.contributor2.name)
+	}
+}
+
+func TestCalcPairings3Contributors(t *testing.T) {
+	benni := Contributor{"Benni"}
+	zoe := Contributor{"Zoé"}
+	dani := Contributor{"Dani"}
+	contributors := Contributors{}
+	contributors.items = append(contributors.items, benni)
+	contributors.items = append(contributors.items, dani)
+	contributors.items = append(contributors.items, zoe)
+
+	pairings := calcPairingsGroup(contributors)
+
+	TestingUtils.CheckEquals(3, len(pairings), t)
+	pair := pairings[0]
+	if (pair.contributor1.name != "Benni") || (pair.contributor2.name != "Zoé") {
+		t.Errorf("first contriburor should be Benni (but was %s) and second should be Dani (but was %s)", pair.contributor1.name, pair.contributor2.name)
+	}
+	pair = pairings[1]
+	if (pair.contributor1.name != "Zoé") || (pair.contributor2.name != "Dani") {
+		t.Errorf("first contriburor should be Zoé (but was %s) and second should be Dani (but was %s)", pair.contributor1.name, pair.contributor2.name)
+	}
+	pair = pairings[2]
+	if (pair.contributor1.name != "Benni") || (pair.contributor2.name != "Dani") {
+		t.Errorf("first contriburor should be Benni (but was %s) and second should be Dani (but was %s)", pair.contributor1.name, pair.contributor2.name)
+	}
 }
